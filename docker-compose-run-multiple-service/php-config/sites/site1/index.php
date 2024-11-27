d6ded60b8ea3c20fdaaca2ad87d274b514669316
hoanguyencoder <thanhhoacth2013@gmail.com>
2024-11-27 16:09:43 +0700

    docker compose multiple service

---
+++ docker-compose-run-multiple-service/php-config/sites/site1/index.php
@@ -0,0 +1,21 @@
+<?php 
+    phpinfo();
+    exit;
+
+$servername = "mysql";  // Tên dịch vụ của MySQL trong Docker Compose
+$username = "hoacode";
+$password = "12345678";
+$dbname = "db_hoacode";
+
+// Kết nối đến MySQL
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+echo json_encode(array("message" => "Hello from site1"));
+
+
+?>
